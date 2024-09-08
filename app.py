2684a7e06733289d7e0fe82e3037a7e6fd77a8cb
Achilles <achilles.oliveira.souza@gmail.com.com>
2024-09-08 15:18:36 -0400

    Adiciona botão de logout e atualiza estilos e templates

--- app.py
+++ app.py
@@ -34,4 +34,4 @@ def submit():
     return render_template('result.html', nome=nome, raca=raca, idade=idade, peso=peso, vacinado=vacinado)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
